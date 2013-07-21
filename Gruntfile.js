cded269901e6a65a9412ad15ab9d3eeb123f1a04
Maruf Siddiqui <maruf@newscred.com>
2013-07-21 13:18:12 +0600

    Jitsu setup add

--- Gruntfile.js
+++ Gruntfile.js
@@ -297,8 +297,8 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    'jshint',
-    'test',
+    //'jshint',
+    //'test',
     'coffee',
     'compass:dist',
     'useminPrepare',
